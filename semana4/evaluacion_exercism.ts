cefe1822e8429d7e9bebbda190a61959abd9d6e2
Brunetob <116246756+Brunetob@users.noreply.github.com>
2023-10-27 12:55:24 -0500

    cm10

--- semana4/evaluacion_exercism.ts
+++ semana4/evaluacion_exercism.ts
@@ -4,7 +4,19 @@
  */
 function decodedValue(colors: string[]): number {
 
-    const colorMap: Record<string, number> = {
+    enum ColorMap{
+        'black' = 0,
+        'brown' = 1,
+        'red'= 2,
+        'orange' = 3,
+        'yellow' = 4,
+        'green' = 5,
+        'blue' = 6,
+        'violet'= 7,
+        'grey' = 8,
+        'white' = 9
+    }
+    /*const colorMap: Record<string, number> = {
       'black': 0,
       'brown': 1,
       'red': 2,
@@ -15,14 +27,13 @@ function decodedValue(colors: string[]): number {
       'violet': 7,
       'grey': 8,
       'white': 9,
-    };
+    };*/
   
     let firstColor = colors[0];
     let secondColor = colors[1];
-  
-    
-    let value = colorMap[firstColor] * 10 + colorMap[secondColor];
-  
+
+    let value = ColorMap[firstColor] * 10 + ColorMap[secondColor];
+
     return value;
 }
 
