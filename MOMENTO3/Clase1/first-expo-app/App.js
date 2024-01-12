96712b4274962119fa3ab87f6f64a1289b3602a8
Brunetob <116246756+Brunetob@users.noreply.github.com>
2024-01-12 08:53:12 -0500

    m3ClaseComponents

--- MOMENTO3/Clase1/first-expo-app/App.js
+++ MOMENTO3/Clase1/first-expo-app/App.js
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import ImageViewer from  './components/ImageViewer'; // Clase 12/01/2024
+import Button from './components/Button';
+
+// Clase 12/101/2024
+// IMportar una imagen
+const PlaceholderImage = require('./assets/images/background-image.jpeg');
+
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{color: '#D94626'}}>Open up App.js to start working on your app!</Text>
-      <Text>Mensjae de prueba</Text>
-      <Text style={styles.text}>Los cambios son instantáneos</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource = {PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -21,6 +33,19 @@ const styles = StyleSheet.create({
     textDecorationColor: '#D94626'
   },
   text: {
-    color: '#AAD926',// Cambiar el color del texto aquí
+    color: '#AAD926',// Se cambia el color de texto
+  },
+  imageContainer: { // Clases 12/01/2024
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: { // Clases 12/01/2024
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+  footerContainer: { // Clases 12/01/2024
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
